d2dbb634b73eda63ec98cf2ec3cc82a49966edc1
比诺 <bi_nuo@hunliji.com>
2019-08-22 23:53:44 +0800

    Vue-SSR官网学习-源码结构

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const App = require('./App');
 module.exports = function createApp(context) {
   return new Vue({
     // 根实例简单的渲染应用程序组件。
-    // render: h => h(App), //render失效
+    // render: h => h(App),
     data: {
       url: context.url
     },
